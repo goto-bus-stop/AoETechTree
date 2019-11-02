cabeb99829d92dba504aa194f000665ab078fc77
Renée Kooi <renee@kooi.me>
2019-11-02 12:12:50 +0100

    Conditionally define NOMINMAX, WIN32_LEAN_AND_MEAN

--- TechTree/GameType.h
+++ TechTree/GameType.h
@@ -3,7 +3,12 @@
 /* INCLUDES */
 
 // Windows main header
+#ifndef NOMINMAX
 #define NOMINMAX
+#endif
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
 #include <windows.h>
 
 // Code cave functions
--- TechTree/functions.h
+++ TechTree/functions.h
@@ -6,7 +6,12 @@
 /* INCLUDES */
 
 // Windows definitions
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
+#endif
 #include <windows.h>
 
 // C standard I/O functions
